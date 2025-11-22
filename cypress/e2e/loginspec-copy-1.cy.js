3b37c6175ad617d04c6ac5c53c8d8ce797a7a676
Andre Silva <andreperer4@gmail.com>
2025-11-22 17:43:04 -0300

    First commit

---
+++ cypress/e2e/loginspec-copy-1.cy.js
@@ -0,0 +1,9 @@
+describe('Orange HRM tests', () => {
+  it('Login com sucesso', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
+  })
+  })
