ffe57fbb45050fcdd5a2e5a8ce395b6eea5cef63
Andre <andreperer4@gmail.com>
2025-11-22 20:56:36 -0300

    base url added

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -12,7 +12,7 @@ describe('Orange HRM tests', () => {
   }
 
   it('Login com sucesso', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username )
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password )
     cy.get(selectorsList.loginButton).click()
@@ -20,7 +20,7 @@ describe('Orange HRM tests', () => {
     cy.get(selectorsList.dashboardgrid) 
   })
   it('Login com falha', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username )
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
