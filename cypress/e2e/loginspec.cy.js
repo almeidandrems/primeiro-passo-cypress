b60391a6c1a2351bb355be31dec4f0edcd750842
Andre <andreperer4@gmail.com>
2025-11-23 12:58:23 -0300

    User update scenario added

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -1,3 +1,4 @@
+import { first, last } from 'lodash'
 import userData from '../fixtures/userData.json'
 
 describe('Orange HRM tests', () => {
@@ -9,16 +10,38 @@ describe('Orange HRM tests', () => {
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardgrid: ".orangehrm-dashboard-grid",
     wrongCredentialsAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dataField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']"
   }
 
-  it('Login com sucesso', () => {
+  it.only('User info Update - Login com sucesso', () => {
+
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username )
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password )
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardgrid) 
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstName')
+    cy.get(selectorsList.lastNameField).clear().type('LastName')
+    cy.get(selectorsList.genericField).eq(3).clear().type('nickname')
+    cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
+    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2025-12-12')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericField).eq(7).clear().type('2025-12-12')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain.text', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
   })
+
   it('Login com falha', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username )
